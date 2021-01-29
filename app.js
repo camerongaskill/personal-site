b5693c7689797b20240c41f64fca66a7991817a2
Cameron Gaskill <camerongaskill@gmail.com>
2021-01-29 17:36:00 -0500

    Added gsap

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 /* eslint-disable indent */
+gsap.registerPlugin(ScrollTrigger);
+
+const project = document.querySelector('.project');
+
+gsap.to(project, {
+	scrollTrigger: {
+		trigger: project,
+		start: 'top center',
+		toggleActions: 'restart none none pause',
+	},
+	x: 200,
+	duration: 2,
+});
