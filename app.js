1a8d6b8c93c86ab6d8f597f45ceabdb58393f401
Cameron Gaskill <camerongaskill@gmail.com>
2021-01-29 23:05:48 -0500

    Added desktop styling

--- app.js
+++ app.js
@@ -10,40 +10,40 @@ const project4 = document.querySelector('#project-4');
 gsap.from(project1, {
 	scrollTrigger: {
 		trigger: project1,
-		start: 'top 60%',
+		start: 'center 60%',
 		toggleActions: 'restart none none reverse',
 	},
-	x: -300,
+	x: -50,
 	opacity: 0,
 	duration: 1.3,
 });
 gsap.from(project2, {
 	scrollTrigger: {
 		trigger: project2,
-		start: 'top 60%',
+		start: 'center 60%',
 		toggleActions: 'restart none none reverse',
 	},
-	x: -300,
+	x: -50,
 	opacity: 0,
 	duration: 1.3,
 });
 gsap.from(project3, {
 	scrollTrigger: {
 		trigger: project3,
-		start: 'top 60%',
+		start: 'center 60%',
 		toggleActions: 'restart none none reverse',
 	},
-	x: -300,
+	x: -50,
 	opacity: 0,
 	duration: 1.3,
 });
 gsap.from(project4, {
 	scrollTrigger: {
 		trigger: project4,
-		start: 'top 60%',
+		start: 'center 60%',
 		toggleActions: 'restart none none reverse',
 	},
-	x: -300,
+	x: -50,
 	opacity: 0,
 	duration: 1.3,
 });
