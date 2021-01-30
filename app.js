5bd7383f619dc80685bf5f582308ec4eead61c73
Cameron Gaskill <camerongaskill@gmail.com>
2021-01-29 19:24:44 -0500

    Projects now animate in and out when scrolling

--- app.js
+++ app.js
@@ -1,14 +1,49 @@
 /* eslint-disable indent */
+// eslint-disable-next-line no-undef
 gsap.registerPlugin(ScrollTrigger);
 
-const project = document.querySelector('.project');
+const project1 = document.querySelector('#project-1');
+const project2 = document.querySelector('#project-2');
+const project3 = document.querySelector('#project-3');
+const project4 = document.querySelector('#project-4');
 
-gsap.to(project, {
+gsap.from(project1, {
 	scrollTrigger: {
-		trigger: project,
-		start: 'top center',
-		toggleActions: 'restart none none pause',
+		trigger: project1,
+		start: 'top 60%',
+		toggleActions: 'restart none none reverse',
 	},
-	x: 200,
-	duration: 2,
+	x: -300,
+	opacity: 0,
+	duration: 1.3,
+});
+gsap.from(project2, {
+	scrollTrigger: {
+		trigger: project2,
+		start: 'top 60%',
+		toggleActions: 'restart none none reverse',
+	},
+	x: -300,
+	opacity: 0,
+	duration: 1.3,
+});
+gsap.from(project3, {
+	scrollTrigger: {
+		trigger: project3,
+		start: 'top 60%',
+		toggleActions: 'restart none none reverse',
+	},
+	x: -300,
+	opacity: 0,
+	duration: 1.3,
+});
+gsap.from(project4, {
+	scrollTrigger: {
+		trigger: project4,
+		start: 'top 60%',
+		toggleActions: 'restart none none reverse',
+	},
+	x: -300,
+	opacity: 0,
+	duration: 1.3,
 });
