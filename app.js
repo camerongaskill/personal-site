80ac41238499c276409e84433a52a15294398c49
Cameron Gaskill <camerongaskill@gmail.com>
2021-01-31 11:19:19 -0500

    Added cool loading animation

--- app.js
+++ app.js
@@ -6,6 +6,24 @@ const project1 = document.querySelector('#project-1');
 const project2 = document.querySelector('#project-2');
 const project3 = document.querySelector('#project-3');
 const project4 = document.querySelector('#project-4');
+const loadingIcon = document.querySelector('.loading-icon');
+const loadingScreenTop = document.querySelector('.loading-top');
+const loadingScreenBot = document.querySelector('.loading-bottom');
+
+const tl = new TimelineMax();
+
+tl.fromTo(loadingIcon, 0.8, { opacity: 0 }, { opacity: 1 })
+	.fromTo(loadingIcon, 0.8, { opacity: 1 }, { opacity: 0 })
+	.fromTo(loadingIcon, 0.8, { opacity: 0 }, { opacity: 1 })
+	.fromTo(loadingIcon, 0.8, { opacity: 1 }, { opacity: 0 })
+	.fromTo(loadingScreenTop, 1, { height: '50vh' }, { height: 0 })
+	.fromTo(
+		loadingScreenBot,
+		1,
+		{ top: '49vh' },
+		{ top: '101vh', display: 'none' },
+		'-=1'
+	);
 
 gsap.from(project1, {
 	scrollTrigger: {
